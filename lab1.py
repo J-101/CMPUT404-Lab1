92472f52b0930f463d75ce59e13fa11df48d56fd
Jake Hennig <jkhennig@cs.ualberta.ca>
2023-09-18 23:15:57 -0600

    Modify to print Git source code

--- lab1.py
+++ lab1.py
@@ -2,6 +2,9 @@ import requests
 
 # print(requests.__version__)
 
-resp = requests.get("http://google.com")
+# resp = requests.get("http://google.com")
 
-print(resp.text)
+# print(resp.text)
+
+resp2 = requests.get("https://raw.githubusercontent.com/J-101/CMPUT404-Lab1/main/lab1.py")
+print(resp2)
